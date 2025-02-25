d5de51c42da304ff04ad295094c8c4e847876fef
dennissammy77 <dennissammy77@gmail.com>
2025-02-25 13:40:17 +0300

    Working with Objects

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = 'Sam';
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+  const new_employee = {...employee};
+  new_employee[key]=value;
+  console.log(new_employee,employee)
+  return new_employee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+  employee[key]=value;
+  return employee;
+};
+
+
+function deleteFromEmployeeByKey(employee,key){
+  const new_employee = {...employee};
+  delete new_employee[key];
+  return new_employee
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  delete employee[key];
+  return employee
+};
